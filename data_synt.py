4d645d1f6c360dd5c5766eb70304f63ab7ea37cc
bagusJatiPramono <135649100+bagusJatiPramono@users.noreply.github.com>
2025-07-20 18:14:54 +0700

    Update data_synt.py
    library nya diupdate rek, kodenya berubah

--- data_synt.py
+++ data_synt.py
@@ -8,13 +8,13 @@ data = pd.read_csv('Data\credit_risk_dataset.csv')
 
 # buat metadata
 metadata = Metadata()
-metadata.detect_from_dataframe(data)
+the_metadata = metadata.detect_from_dataframe(data)
 
 # buat sintesis data
-synthesizer = GaussianCopulaSynthesizer(metadata)
-synthesizer.fit(data=data)
+synthesizer = GaussianCopulaSynthesizer(the_metadata)
+synthesizer.fit(data)
 
-synthetic_data = synthesizer.sample(num_rows=500)
+synthetic_data = synthesizer.sample(num_rows=1000)
 
 # simpan data baru
 previous_data = data.copy()
